1e81e2ca50411320bcf0eef6c1b57197ae6abf6b
asnidea <asnidea@gmail.com>
2017-07-20 22:42:57 +0800

    Microsoft.PowerShell_profile.ps1

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,10 @@
+# path: \My Documents\WindowsPowerShell
+
+Import-Module posh-docker
+
+
+# Set-Alias
+Set-Alias dk docker
+Set-Alias dm docker-machine
+Set-Alias dc docker-compose
+#Set-Alias fig docker-compose
